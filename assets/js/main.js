13d7e289ae451849ed9274de2a3d7483cc7ec393
Menno Engels <menno.engels@live.nl>
2023-04-18 00:55:19 +0200

    Add smooth scroll behavior to navigation links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
+// Select all navigation links and page elements
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// Add click event listener to each navigation link
+navigationLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		// Loop through all pages and find the one that matches the link's data-page attribute
+		pages.forEach((page) => {
+			if (link.innerHTML.toLowerCase() === page.dataset.page) {
+				// Scroll to the page using smooth scroll behavior
+				window.scrollTo({
+					top: page.offsetTop,
+					behavior: 'smooth',
+				});
+			}
+		});
+	});
+});
