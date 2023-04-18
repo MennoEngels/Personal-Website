2076c93ab5974994fd4d2b27e823c95b4823c4b5
Menno Engels <menno.engels@live.nl>
2023-04-19 00:21:48 +0200

    Logic for selecting & filtering project categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,65 @@
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
+// variables for custom select
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+// Select all filter items
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
 // Select the contact form and its elements
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
+
+// function for toggling element class
+const toggleClass = function (elem) {
+	elem.classList.toggle("active");
+}
+
+// event listener for select element
+select.addEventListener("click", function () {
+	toggleClass(this);
+});
+
+// Select the filter items and define the filterFunc to show or hide items based on selected category
+const filterFunc = (selectedValue) => {
+	filterItems.forEach((item) => {
+		if (selectedValue === "all") {
+			item.classList.add("active");
+		} else if (selectedValue === item.dataset.category) {
+			item.classList.add("active");
+		} else {
+			item.classList.remove("active");
+		}
+	});
+};
+
+// Add click event listener to each select item and update the selected value and filter the items
+selectItems.forEach((item) => {
+	item.addEventListener("click", () => {
+		const selectedValue = item.innerText.toLowerCase();
+		selectValue.innerText = item.innerText;
+		elementToggleFunc(select);
+		filterFunc(selectedValue);
+	});
+});
+
+// Add click event listener to each filter button and update the selected value and filter the items
+let lastClickedBtn = filterBtn[0];
+filterBtn.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		const selectedValue = btn.innerText.toLowerCase();
+		selectValue.innerText = btn.innerText;
+		filterFunc(selectedValue);
+		lastClickedBtn.classList.remove("active");
+		btn.classList.add("active");
+		lastClickedBtn = btn;
+	});
+});
 
 // Add input event listener to each form input field and validate the form
 formInputs.forEach((input) => {
