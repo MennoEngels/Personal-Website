c9bad9f5409a8c00667534f9aab5a8cf5724e14e
Menno Engels <menno.engels@live.nl>
2023-04-18 14:32:21 +0200

    Add form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,22 @@
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
+// Select the contact form and its elements
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// Add input event listener to each form input field and validate the form
+formInputs.forEach((input) => {
+	input.addEventListener("input", () => {
+		if (validateForm()) {
+			formBtn.removeAttribute("disabled");
+		} else {
+			formBtn.setAttribute("disabled", "");
+		}
+	});
+});
+
 // Add click event listener to each navigation link
 navigationLinks.forEach((link) => {
 	link.addEventListener("click", () => {
@@ -19,3 +35,18 @@ navigationLinks.forEach((link) => {
 		});
 	});
 });
+
+// Function for validating form
+const validateForm = () => {
+	const { fullname, email, message } = form.elements;
+
+	const isEmailValid = email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+	const isMessageValid = message.value.length < 1000;
+	const isNameValid = fullname.value.length < 100;
+
+	if (form.checkValidity() && isEmailValid && isMessageValid && isNameValid) {
+		return true;
+	} else {
+		return false;
+	}
+};
